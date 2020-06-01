ec46fee40d91ab6d39ccdd0e352f2159e62e984c
Atherton <joelblak3@gmail.com>
2020-06-01 16:44:34 +0200

    Segundo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <div class="container">
                 <a class="navbar-brand nlogo" href="{{ url('/') }}">
-                    €$CIN
+                    €$CINss
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
